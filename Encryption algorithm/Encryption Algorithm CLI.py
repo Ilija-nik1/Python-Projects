eab7d4d59be82514b71d3a23d3c0b40fda77f2e1
I_N-01 <110109065+Ilija-nik1@users.noreply.github.com>
2023-03-25 11:55:07 +0100

    Add files via upload

--- Encryption algorithm/Encryption Algorithm CLI.py
+++ Encryption algorithm/Encryption Algorithm CLI.py
@@ -1,37 +1,71 @@
 import base64
 import os
 from Cryptodome.Cipher import AES
+import getpass
 
-# Prompt the user for a message and a password
-message = input("Enter a message to encrypt: ")
-password = input("Enter a password: ")
+def encrypt_message(message: str, password: str) -> bytes:
+    try:
+        # Use a key derivation function to generate a key from the password
+        salt = os.urandom(16)
+        key = get_key(password, salt)
 
-# Convert the password to bytes and use it as the key for AES
-key = password.encode()
+        # Generate a random initialization vector (IV)
+        iv = os.urandom(AES.block_size)
 
-# Generate a random initialization vector (IV)
-iv = os.urandom(AES.block_size)
+        # Create a new AES cipher object in CBC mode
+        cipher = AES.new(key, AES.MODE_CBC, iv)
 
-# Create a new AES cipher object in CBC mode
-cipher = AES.new(key, AES.MODE_CBC, iv)
+        # Encrypt the message
+        ciphertext = cipher.encrypt(bytes(message, 'utf-8'))
 
-# Encrypt the message
-ciphertext = cipher.encrypt(bytes(message, 'utf-8'))
+        # Encode the ciphertext and IV as base64 strings
+        ciphertext_b64 = base64.b64encode(ciphertext).decode()
+        iv_b64 = base64.b64encode(iv).decode()
 
-# Encode the ciphertext and IV as base64 strings
-ciphertext_b64 = base64.b64encode(ciphertext).decode()
-iv_b64 = base64.b64encode(iv).decode()
+        # Return the encrypted message, salt, and IV
+        return b'%b:%b:%b' % (salt, iv_b64.encode(), ciphertext_b64.encode())
 
-# Display the encrypted message
-print(f"Encrypted message: {ciphertext_b64}")
-print(f"IV: {iv_b64}")
+    except (ValueError, TypeError) as e:
+        print(f"Error encrypting message: {e}")
 
-# To decrypt the message, we need to recreate the cipher object
-# using the same key and IV
-cipher = AES.new(key, AES.MODE_CBC, iv)
+def decrypt_message(encrypted_message: bytes, password: str) -> str:
+    try:
+        # Split the encrypted message into its components
+        salt, iv_b64, ciphertext_b64 = encrypted_message.split(b':')
 
-# Decrypt the ciphertext
-plaintext = cipher.decrypt(ciphertext)
+        # Use a key derivation function to generate a key from the password and salt
+        key = get_key(password, salt)
 
-# Display the decrypted message
-print(f"Decrypted message: {plaintext.decode('utf-8')}")
+        # Decode the IV and ciphertext from base64
+        iv = base64.b64decode(iv_b64)
+        ciphertext = base64.b64decode(ciphertext_b64)
+
+        # Create a new AES cipher object in CBC mode
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+
+        # Decrypt the message
+        plaintext = cipher.decrypt(ciphertext)
+
+        # Return the decrypted message
+        return plaintext.decode('utf-8')
+
+    except (ValueError, TypeError) as e:
+        print(f"Error decrypting message: {e}")
+
+def get_key(password: str, salt: bytes) -> bytes:
+    # Use PBKDF2 to generate a key from the password and salt
+    key = getpass.pbkdf2(password, salt, 100000, 32)
+    return key
+
+if __name__ == '__main__':
+    # Prompt the user for a message and a password
+    message = input("Enter a message to encrypt: ")
+    password = getpass.getpass(prompt="Enter a password: ")
+
+    # Encrypt the message
+    encrypted_message = encrypt_message(message, password)
+    print(f"Encrypted message: {encrypted_message}")
+
+    # Decrypt the message
+    decrypted_message = decrypt_message(encrypted_message, password)
+    print(f"Decrypted message: {decrypted_message}")
--- Encryption algorithm/Encryption Algorithm GUI.py
+++ Encryption algorithm/Encryption Algorithm GUI.py
@@ -1,91 +1,125 @@
 import string
 import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.simpledialog
+import tkinter.filedialog
 import pyperclip
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
-# Caesar Cipher encryption function
+# AES encryption function
 def encrypt(text, key):
-    result = ""
-    for char in text:
-        if char.isalpha():
-            char_index = (string.ascii_lowercase.index(char.lower()) + key) % 26
-            if char.isupper():
-                result += string.ascii_uppercase[char_index]
-            else:
-                result += string.ascii_lowercase[char_index]
-        else:
-            result += char
-    return result
+    key = key.encode("utf-8")  # Convert password to bytes
+    cipher = AES.new(key, AES.MODE_CBC)
+    ciphertext, iv = cipher.encrypt_and_digest(pad(text.encode("utf-8"), AES.block_size))
+    return iv + ciphertext
 
-# Caesar Cipher decryption function
+# AES decryption function
 def decrypt(ciphertext, key):
-    result = ""
-    for char in ciphertext:
-        if char.isalpha():
-            char_index = (string.ascii_lowercase.index(char.lower()) - key) % 26
-            if char.isupper():
-                result += string.ascii_uppercase[char_index]
-            else:
-                result += string.ascii_lowercase[char_index]
-        else:
-            result += char
-    return result
+    key = key.encode("utf-8")  # Convert password to bytes
+    iv = ciphertext[:AES.block_size]
+    ciphertext = ciphertext[AES.block_size:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    return plaintext.decode("utf-8")
 
 # Event handler for encrypt button
 def encrypt_message():
     plaintext = plaintext_input.get()
-    password = password_input.get()
-    key = sum(ord(char) for char in password)
-    ciphertext = encrypt(plaintext, key)
-    ciphertext_display.configure(text=ciphertext)
+    password = tkinter.simpledialog.askstring("Password", "Enter a password to use for encryption:", show='*')
+    if password:
+        ciphertext = encrypt(plaintext, password)
+        ciphertext_display.configure(text=ciphertext.hex())
 
 # Event handler for decrypt button
 def decrypt_message():
-    ciphertext = ciphertext_display.cget("text")
-    password = password_input.get()
-    key = sum(ord(char) for char in password)
-    plaintext = decrypt(ciphertext, key)
-    plaintext_display.configure(text=plaintext)
+    ciphertext_hex = ciphertext_display.cget("text")
+    password = tkinter.simpledialog.askstring("Password", "Enter the password used for encryption:", show='*')
+    if password:
+        if ciphertext_hex:
+            try:
+                ciphertext = bytes.fromhex(ciphertext_hex)
+                plaintext = decrypt(ciphertext, password)
+                plaintext_display.configure(text=plaintext)
+            except ValueError:
+                tk.messagebox.showerror("Error", "Invalid ciphertext")
 
 # Event handler for copy button
 def copy_text():
-    if ciphertext_display.cget("text"):
-        pyperclip.copy(ciphertext_display.cget("text"))
-    elif plaintext_display.cget("text"):
-        pyperclip.copy(plaintext_display.cget("text"))
+    text = ciphertext_display.cget("text") or plaintext_display.cget("text")
+    if text:
+        pyperclip.copy(text)
+
+# Event handler for encrypt file button
+def encrypt_file():
+    password = tkinter.simpledialog.askstring("Password", "Enter a password to use for encryption:", show='*')
+    if password:
+        filepath = tkinter.filedialog.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                plaintext = f.read()
+            ciphertext = encrypt(plaintext, password)
+            with open(filepath + '.enc', 'wb') as f:
+                f.write(ciphertext)
+
+# Event handler for decrypt file button
+def decrypt_file():
+    password = tkinter.simpledialog.askstring("Password", "Enter the password used for encryption:", show='*')
+    if password:
+        filepath = tkinter.filedialog.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                ciphertext = f.read()
+            try:
+                plaintext = decrypt(ciphertext, password)
+                with open(filepath[:-4], 'wb') as f:
+                    f.write(plaintext)
+            except ValueError:
+                tk.messagebox.showerror("Error", "Invalid ciphertext")
 
 # Create the main window
 root = tk.Tk()
-root.title("Caesar Cipher Encryption")
+root.title("AES Encryption")
 
 # Create the input labels and entry widgets
-plaintext_label = tk.Label(root, text="Enter the message you want to encrypt:")
-plaintext_input = tk.Entry(root)
-password_label = tk.Label(root, text="Enter a password to use for encryption:")
-password_input = tk.Entry(root, show="*")
+plaintext_label = ttk.Label(root, text="Enter the message you want to encrypt:")
+plaintext_input = ttk.Entry(root)
+password_label = ttk.Label(root, text="Enter a password to use for encryption:")
+password_input = ttk.Entry(root, show="*")
 
 # Create the encrypt, decrypt, and copy buttons
-encrypt_button = tk.Button(root, text="Encrypt", command=encrypt_message)
-decrypt_button = tk.Button(root, text="Decrypt", command=decrypt_message)
-copy_button = tk.Button(root, text="Copy", command=copy_text)
+encrypt_button = ttk.Button(root, text="Encrypt", command=encrypt_message)
+decrypt_button = ttk.Button(root, text="Decrypt", command=decrypt_message)
+copy_button = ttk.Button(root, text="Copy", command=copy_text)
 
-# Create the output labels
-ciphertext_label = tk.Label(root, text="Encrypted message:")
-ciphertext_display = tk.Label(root, text="")
-plaintext_label2 = tk.Label(root, text="Decrypted message:")
-plaintext_display = tk.Label(root, text="")
+#Create the ciphertext and plaintext display labels
+ciphertext_label = ttk.Label(root, text="Ciphertext:")
+ciphertext_display = ttk.Label(root, wraplength=400)
+plaintext_label2 = ttk.Label(root, text="Plaintext:")
+plaintext_display = ttk.Label(root, wraplength=400)
 
-# Pack the widgets into the window
-plaintext_label.pack()
-plaintext_input.pack()
-password_label.pack()
-password_input.pack()
-encrypt_button.pack()
-decrypt_button.pack()
-copy_button.pack()
-ciphertext_label.pack()
-ciphertext_display.pack()
-plaintext_label2.pack()
-plaintext_display.pack()
+#Create the file encryption and decryption buttons
+encrypt_file_button = ttk.Button(root, text="Encrypt File", command=encrypt_file)
+decrypt_file_button = ttk.Button(root, text="Decrypt File", command=decrypt_file)
 
-# Start the main event loop
+#Set the layout using the grid geometry manager
+plaintext_label.grid(column=0, row=0, padx=5, pady=5, sticky="w")
+plaintext_input.grid(column=0, row=1, padx=5, pady=5, sticky="ew")
+password_label.grid(column=0, row=2, padx=5, pady=5, sticky="w")
+password_input.grid(column=0, row=3, padx=5, pady=5, sticky="ew")
+encrypt_button.grid(column=0, row=4, padx=5, pady=5)
+decrypt_button.grid(column=0, row=5, padx=5, pady=5)
+copy_button.grid(column=0, row=6, padx=5, pady=5)
+ciphertext_label.grid(column=0, row=7, padx=5, pady=5, sticky="w")
+ciphertext_display.grid(column=0, row=8, padx=5, pady=5, sticky="ew")
+plaintext_label2.grid(column=0, row=9, padx=5, pady=5, sticky="w")
+plaintext_display.grid(column=0, row=10, padx=5, pady=5, sticky="ew")
+encrypt_file_button.grid(column=0, row=11, padx=5, pady=5)
+decrypt_file_button.grid(column=0, row=12, padx=5, pady=5)
+
+#Set the window size and disable resizing
+root.geometry("500x600")
+root.resizable(False, False)
+
+#Start the main event loop
 root.mainloop()
