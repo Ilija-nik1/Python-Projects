4a8f70d549b8d135e75ec2a27472ddc180eec97b
I_N-01 <110109065+Ilija-nik1@users.noreply.github.com>
2023-03-25 11:59:01 +0100

    Add files via upload

--- Encryption algorithm/Encryption Algorithm GUI.py
+++ Encryption algorithm/Encryption Algorithm GUI.py
@@ -92,17 +92,17 @@ encrypt_button = ttk.Button(root, text="Encrypt", command=encrypt_message)
 decrypt_button = ttk.Button(root, text="Decrypt", command=decrypt_message)
 copy_button = ttk.Button(root, text="Copy", command=copy_text)
 
-#Create the ciphertext and plaintext display labels
+# Create the ciphertext and plaintext display labels
 ciphertext_label = ttk.Label(root, text="Ciphertext:")
 ciphertext_display = ttk.Label(root, wraplength=400)
 plaintext_label2 = ttk.Label(root, text="Plaintext:")
 plaintext_display = ttk.Label(root, wraplength=400)
 
-#Create the file encryption and decryption buttons
+# Create the file encryption and decryption buttons
 encrypt_file_button = ttk.Button(root, text="Encrypt File", command=encrypt_file)
 decrypt_file_button = ttk.Button(root, text="Decrypt File", command=decrypt_file)
 
-#Set the layout using the grid geometry manager
+# Set the layout using the grid geometry manager
 plaintext_label.grid(column=0, row=0, padx=5, pady=5, sticky="w")
 plaintext_input.grid(column=0, row=1, padx=5, pady=5, sticky="ew")
 password_label.grid(column=0, row=2, padx=5, pady=5, sticky="w")
@@ -117,9 +117,9 @@ plaintext_display.grid(column=0, row=10, padx=5, pady=5, sticky="ew")
 encrypt_file_button.grid(column=0, row=11, padx=5, pady=5)
 decrypt_file_button.grid(column=0, row=12, padx=5, pady=5)
 
-#Set the window size and disable resizing
+# Set the window size and disable resizing
 root.geometry("500x600")
 root.resizable(False, False)
 
-#Start the main event loop
+# Start the main event loop
 root.mainloop()
