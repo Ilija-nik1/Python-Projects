f74edcea4863a2e187c7661a67991c8699d98a49
I_N-01 <110109065+Ilija-nik1@users.noreply.github.com>
2023-03-29 20:19:29 +0200

    Add files via upload

--- Number Converter.py
+++ Number Converter.py
@@ -1,42 +1,53 @@
 from tkinter import *
 from tkinter import messagebox
 
+class Converter:
+    # Define constants
+    WINDOW_TITLE = "Decimal to Binary/Octal/Hexadecimal Converter"
+    LABEL_TEXT = "Enter your Decimal:"
+    CONVERT_BUTTON_TEXT = "Convert"
+    RESULT_LABEL_TEXT = "Decimal: {}\nBinary: {}\nOctal: {}\nHexadecimal: {}"
+    
+    def __init__(self, master):
+        self.master = master
+        master.title(self.WINDOW_TITLE)
+        
+        # Designing window for registration
+        self.label1 = Label(master, text=self.LABEL_TEXT)
+        self.label1.grid(row=0, column=0)
+
+        self.dec_entry = Entry(master)
+        self.dec_entry.grid(row=0, column=1)
+
+        # Submit Button
+        self.submit_button = Button(master, text=self.CONVERT_BUTTON_TEXT, command=self.convert)
+        self.submit_button.grid(row=1, columnspan=2)
+
+        # Label to display the result
+        self.result_label = Label(master, text="")
+        self.result_label.grid(row=2, columnspan=2)
+
+    def convert(self):
+        # Get the value from Entry field
+        try:
+            dec_value = int(self.dec_entry.get())
+            if dec_value < 0:
+                raise ValueError
+        except ValueError:
+            messagebox.showerror("Error", "Please enter a positive integer.")
+            return
+
+        # Convert to binary, octal, and hexadecimal
+        bin_value = bin(dec_value)[2:]
+        oct_value = oct(dec_value)[2:]
+        hex_value = hex(dec_value)[2:]
+
+        # Display the result in a label
+        self.result_label.config(text=self.RESULT_LABEL_TEXT.format(dec_value, bin_value, oct_value, hex_value))
+
 # Create window object
 root = Tk()
-root.title("Decimal to Binary/Octal/Hexadecimal Converter")
-
-# Function to convert decimal to binary, octal, and hexadecimal
-def convert():
-    try:
-        # Get the value from Entry field
-        dec_value = int(dec_entry.get())
-    except ValueError:
-        messagebox.showerror("Error", "Please enter a valid decimal number")
-        return
-
-    # Convert to binary, octal, and hexadecimal
-    bin_value = bin(dec_value)[2:]
-    oct_value = oct(dec_value)[2:]
-    hex_value = hex(dec_value)[2:]
-
-    # Display the result in a label
-    result_label.config(text="Decimal: {}\nBinary: {}\nOctal: {}\nHexadecimal: {}"
-                                 .format(dec_value, bin_value, oct_value, hex_value))
-
-# Designing window for registration
-label1 = Label(root, text="Enter your Decimal:")
-label1.grid(row=0, column=0)
-
-dec_entry = Entry(root)
-dec_entry.grid(row=0, column=1)
-
-# Submit Button
-submit_button = Button(root, text="Convert", command=convert)
-submit_button.grid(row=1, columnspan=2)
-
-# Label to display the result
-result_label = Label(root, text="")
-result_label.grid(row=2, columnspan=2)
+converter = Converter(root)
 
 # End the loop when window is closed
 root.mainloop()
