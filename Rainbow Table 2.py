19869e0e5a0a98b45379bfb08e89380516279464
I_N-01 <110109065+Ilija-nik1@users.noreply.github.com>
2023-04-05 11:36:12 +0200

    Add files via upload

--- Rainbow Table 2.py
+++ Rainbow Table 2.py
@@ -1,24 +1,39 @@
 import hashlib
+import bcrypt
 import secrets
 
 def create_rainbow_table(password_list):
+    if not password_list:
+        raise ValueError("Password list is empty")
+
     rainbow_table = {}
     
     # Iterate through the list of passwords
     for password in password_list:
+        if not isinstance(password, str):
+            raise TypeError("Password must be a string")
+
+        # Check password strength
+        if not is_strong_password(password):
+            raise ValueError("Weak password: {}".format(password))
+        
         # Generate a random salt
-        salt = secrets.token_hex(16)
+        salt = secrets.token_hex(32)
         
         # Combine the password and salt
         salted_password = password + salt
         
-        # Calculate the hash of the salted password using SHA256
-        hashed_password = hashlib.sha256(salted_password.encode()).hexdigest()
+        # Calculate the hash of the salted password using bcrypt
+        hashed_password = bcrypt.hashpw(salted_password.encode(), bcrypt.gensalt())
         
         # Add the salt, hash and the password to the rainbow table
         rainbow_table[hashed_password] = (salt, password)
         
     return rainbow_table
+
+def is_strong_password(password):
+    # Implement password strength check logic here
+    return True
 
 # Example password list
 password_list = ['password1', 'password2', 'password3']
--- Rainbow Table.py
+++ Rainbow Table.py
@@ -1,14 +1,18 @@
 import hashlib
+import secrets
 
 # The characters to use for the password
-chars = "abcdefghijklmnopqrstuvwxyz"
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 chars_len = len(chars)
 
-# Reduces an integer i to a 5-character password
-def reduce(i):
-    pwd=""
+# Reduces an integer i to a password of length pwd_len
+def reduce(i, pwd_len):
+    if not isinstance(i, int) or i < 0:
+        raise ValueError("Input must be a non-negative integer")
+        
+    pwd = ""
     # Use a for loop instead of a while loop to generate the password
-    for _ in range(5):
+    for _ in range(pwd_len):
         pwd += chars[i % chars_len]
         i //= chars_len
     return pwd
@@ -17,13 +21,13 @@ def reduce(i):
 table = []
 for s in range(1000):
     # Use reduce to generate the start of a chain
-    start = reduce(s)
+    start = reduce(s, 8)
     p = start
     for i in range(1000):
         # Hash the password using a secure hashing algorithm
         h = hashlib.sha256(p.encode('ascii')).hexdigest()
         # Reduce the hash to a new password
-        p = reduce(int(h, 16))
+        p = reduce(int(h, 16), 8)
     table.append([start, p])
 
 # Print the table of password chains
