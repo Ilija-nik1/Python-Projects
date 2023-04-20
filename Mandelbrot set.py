b770f27166a1ace59bf39a103a0d0aa149441534
I_N-01 <110109065+Ilija-nik1@users.noreply.github.com>
2023-04-20 19:35:59 +0200

    Add files via upload

--- Mandelbrot set.py
+++ Mandelbrot set.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 
 # Define the size of the image
 width, height = 1000, 1000
@@ -24,8 +25,46 @@ for i in range(max_iterations):
     n_iterations[mask] = i
     z[mask] = z[mask]**2 + c[mask]
 
-# Plot the Mandelbrot set using a grayscale colormap
-plt.imshow(n_iterations.T, cmap='gray', extent=(x_min, x_max, y_min, y_max))
+# Create a custom colormap to colorize the plot
+cmap = colors.ListedColormap(['darkblue', 'blue', 'purple', 'red', 'orange', 'yellow', 'white'])
+
+# Plot the Mandelbrot set using a custom colormap
+plt.imshow(n_iterations.T, cmap=cmap, extent=(x_min, x_max, y_min, y_max))
+
+# Add a colorbar to the plot
+plt.colorbar()
+
+# Set the x and y axis labels
 plt.xlabel('Real')
 plt.ylabel('Imaginary')
+
+# Set the limits of the plot
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+
+# Add a zoom function to the plot
+def onclick(event):
+    global x_min, x_max, y_min, y_max
+    zoom_factor = 0.5
+    x_range = x_max - x_min
+    y_range = y_max - y_min
+    x_center = x_min + x_range / 2
+    y_center = y_min + y_range / 2
+    x_new_range = x_range * zoom_factor
+    y_new_range = y_range * zoom_factor
+    x_min = x_center - x_new_range / 2
+    x_max = x_center + x_new_range / 2
+    y_min = y_center - y_new_range / 2
+    y_max = y_center + y_new_range / 2
+    plt.clf()
+    plt.imshow(n_iterations.T, cmap=cmap, extent=(x_min, x_max, y_min, y_max))
+    plt.colorbar()
+    plt.xlabel('Real')
+    plt.ylabel('Imaginary')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+cid = plt.gcf().canvas.mpl_connect('button_press_event', onclick)
+
+# Show the plot
 plt.show()
