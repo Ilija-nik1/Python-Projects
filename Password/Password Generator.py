cf9a854f01814fc32df71893baa34abeece0f425
Ilija_Nik_1 <110109065+Ilija-nik1@users.noreply.github.com>
2023-06-01 13:29:55 +0200

    Add files via upload

--- Password/Password Generator.py
+++ Password/Password Generator.py
@@ -9,29 +9,33 @@ class PasswordGeneratorGUI:
         self.master = master
         master.title("Password Generator")
 
+        # Apply dark theme
+        master.configure(bg="#2b2b2b")  # Set background color
+        master.option_add("*foreground", "white")  # Set foreground (text) color
+
         # Determine maximum password length based on available window width
         max_width = master.winfo_screenwidth() // 2
         max_length = max_width // 9
 
         # Create and place password length label and entry widget
-        self.length_label = tk.Label(master, text="Password length:")
+        self.length_label = tk.Label(master, text="Password length:", bg="#2b2b2b")
         self.length_label.grid(row=0, column=0, sticky="W")
-        self.length_entry = tk.Entry(master, width=max_length)
+        self.length_entry = tk.Entry(master, width=max_length, bg="#424242", fg="white")
         self.length_entry.insert(0, "16")
         self.length_entry.grid(row=0, column=1)
         self.length_label.grid_configure(padx=5, pady=5)
         self.length_entry.grid_configure(padx=5, pady=5)
 
         # Create and place generate button
-        self.generate_button = tk.Button(master, text="Generate", command=self.generate_password)
+        self.generate_button = tk.Button(master, text="Generate", command=self.generate_password, bg="#757575", fg="white")
         self.generate_button.grid(row=1, column=0, columnspan=2, pady=5)
 
         # Create and place output label
-        self.password_label = tk.Label(master, text="")
+        self.password_label = tk.Label(master, text="", bg="#2b2b2b")
         self.password_label.grid(row=2, column=0, columnspan=2, pady=5)
 
         # Create and place copy button
-        self.copy_button = tk.Button(master, text="Copy to Clipboard", command=self.copy_password)
+        self.copy_button = tk.Button(master, text="Copy to Clipboard", command=self.copy_password, bg="#757575", fg="white")
         self.copy_button.grid(row=3, column=0, columnspan=2, pady=5)
 
     def generate_password(self):
--- Password/Password check.py
+++ Password/Password check.py
@@ -32,19 +32,29 @@ class PasswordStrengthGUI:
         self.master = master
         master.title("Password Strength Checker")
 
+        # Configure dark mode theme
+        self.configure_dark_theme()
+
         # Create and place password label and entry widget
-        self.password_label = tk.Label(master, text="Enter password:")
+        self.password_label = tk.Label(master, text="Enter password:", bg="#2b2b2b", fg="white")
         self.password_label.grid(row=0, column=0, sticky="W")
-        self.password_entry = tk.Entry(master, show="*")
+        self.password_entry = tk.Entry(master, show="*", bg="#2b2b2b", fg="white")
         self.password_entry.grid(row=0, column=1)
 
         # Create and place check button
-        self.check_button = tk.Button(master, text="Check", command=self.check_password_strength)
+        self.check_button = tk.Button(master, text="Check", command=self.check_password_strength, bg="#2b2b2b", fg="white")
         self.check_button.grid(row=1, column=0, columnspan=2)
 
         # Create and place output label
-        self.password_strength_label = tk.Label(master, text="")
+        self.password_strength_label = tk.Label(master, text="", bg="#2b2b2b", fg="white")
         self.password_strength_label.grid(row=2, column=0, columnspan=2)
+
+    def configure_dark_theme(self):
+        # Configure dark mode theme settings
+        self.master.config(bg="#2b2b2b")
+        self.master['bg'] = "#2b2b2b"
+        self.master.option_add('*background', '#2b2b2b')
+        self.master.option_add('*foreground', 'white')
 
     def check_password_strength(self):
         # Get password input
