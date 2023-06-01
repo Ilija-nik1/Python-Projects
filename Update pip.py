8df476ddb56caf49e542040dfdd0713c6eedeac3
Ilija_Nik_1 <110109065+Ilija-nik1@users.noreply.github.com>
2023-06-01 11:31:25 +0200

    Add files via upload

--- Update pip.py
+++ Update pip.py
@@ -1,7 +1,19 @@
 import subprocess
+import sys
 
 def update_pip():
-    subprocess.call(["python", "-m", "pip", "install", "--upgrade", "pip"])
+    """
+    Update pip to the latest version using subprocess module.
+    """
+    try:
+        subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip"], check=True)
+        print("Pip has been successfully updated to the latest version.")
+    except subprocess.CalledProcessError as error:
+        print(f"Error while updating pip: {error}")
+        sys.exit(1)
+    except Exception as error:
+        print(f"An error occurred while updating pip: {error}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     update_pip()
