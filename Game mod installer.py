ac23762e4130fc479563f8db9c3c41f9489925b2
Ilija_Nik_1 <110109065+Ilija-nik1@users.noreply.github.com>
2023-07-02 14:31:49 +0200

    Add files via upload

--- Game mod installer.py
+++ Game mod installer.py
@@ -21,6 +21,67 @@ def make_api_request(url):
         print(f"Error while making API request: {e}")
         return None
 
+def get_mod_details(mod_id):
+    url = f"{API_BASE_URL}/mods/{mod_id}"
+    mod_details = make_api_request(url)
+
+    if mod_details:
+        print_mod_details(mod_details)
+    else:
+        print("Mod not found.")
+
+def enable_mod(mod_id, enable=True):
+    mod_config = load_config()
+    if mod_id in mod_config:
+        mod_config[mod_id]['enabled'] = enable
+        save_config(mod_config)
+        status = "enabled" if enable else "disabled"
+        print(f"Mod '{mod_id}' {status} successfully.")
+    else:
+        print("Mod ID not found in the configuration.")
+
+
+def enable_all_mods():
+    mod_config = load_config()
+    for mod_id in mod_config:
+        enable_mod(mod_id, enable=True)
+
+def disable_all_mods():
+    mod_config = load_config()
+    for mod_id in mod_config:
+        enable_mod(mod_id, enable=False)
+
+def delete_mod(mod_id):
+    uninstall_mod(mod_id)
+    mod_config = load_config()
+    if mod_id in mod_config:
+        del mod_config[mod_id]
+        save_config(mod_config)
+        print(f"Mod '{mod_id}' deleted successfully.")
+    else:
+        print("Mod ID not found in the configuration.")
+
+def view_enabled_mods():
+    mod_config = load_config()
+    for mod_id, mod_data in mod_config.items():
+        if mod_data['enabled']:
+            print_mod_details(mod_data)
+
+def view_disabled_mods():
+    mod_config = load_config()
+    for mod_id, mod_data in mod_config.items():
+        if not mod_data['enabled']:
+            print_mod_details(mod_data)
+
+def get_total_installed_mods():
+    mod_config = load_config()
+    total_installed_mods = len(mod_config)
+    print(f"Total installed mods: {total_installed_mods}")
+
+def is_mod_installed(mod_id):
+    mod_config = load_config()
+    return mod_id in mod_config
+
 # Function to search for mods
 def search_mods(query):
     url = f"{API_BASE_URL}/search?query={query}"
@@ -61,166 +122,145 @@ def uninstall_mod(mod_id):
     # Remove the mod file from the mods directory
     mod_config = load_config()
     if mod_id in mod_config:
-        mod_file = mod_config[mod_id]['filename']
+        mod_file = mod_config[mod_id]['file']
         mod_path = os.path.join(MODS_DIRECTORY, mod_file)
+
         if os.path.exists(mod_path):
             os.remove(mod_path)
-            print(f"Mod '{mod_id}' uninstalled successfully!")
-            del mod_config[mod_id]
-            save_config(mod_config)
+            print(f"Mod '{mod_config[mod_id]['name']}' uninstalled successfully!")
         else:
             print("Mod file not found.")
     else:
         print("Mod ID not found in the configuration.")
 
+    # Remove the mod entry from the config
+    if mod_id in mod_config:
+        del mod_config[mod_id]
+        save_config(mod_config)
+
 # Function to update a mod
 def update_mod(mod_id):
-    # Uninstall the current version and install the latest version
+    mod_config = load_config()
+    if mod_id not in mod_config:
+        print("Mod ID not found in the configuration.")
+        return
+
+    mod_details = make_api_request(f"{API_BASE_URL}/mods/{mod_id}")
+    if not mod_details:
+        print("Mod not found.")
+        return
+
+    current_version = mod_config[mod_id]['version']
+    latest_version = mod_details['version']
+
+    if current_version == latest_version:
+        print("Mod is already up to date.")
+        return
+
     uninstall_mod(mod_id)
     install_mod(mod_id)
 
-# Function to view installed mods
-def view_installed_mods():
+def backup_mods(backup_directory):
     mod_config = load_config()
-    for mod_id, mod_data in mod_config.items():
-        print_mod_details(mod_data)
+    if not os.path.exists(backup_directory):
+        os.makedirs(backup_directory)
 
-# Function to check for updates
-def check_mod_updates():
-    mod_config = load_config()
     for mod_id, mod_data in mod_config.items():
-        mod_version = mod_data['version']
-        url = f"{API_BASE_URL}/mods/{mod_id}"
-        latest_mod_details = make_api_request(url)
-        if latest_mod_details and latest_mod_details['version'] > mod_version:
-            print(f"Mod '{mod_data['name']}' has an update available.")
+        mod_file = mod_data['file']
+        mod_path = os.path.join(MODS_DIRECTORY, mod_file)
 
-# Function to download a file (unchanged)
+        if os.path.exists(mod_path):
+            shutil.copy(mod_path, backup_directory)
+        else:
+            print(f"Mod file '{mod_file}' not found. Skipping backup.")
+
+    print("Mods backup completed successfully.")
+
+def load_mods_from_backup(backup_directory):
+    mod_files = os.listdir(backup_directory)
+    for mod_file in mod_files:
+        mod_path = os.path.join(backup_directory, mod_file)
+        if os.path.isfile(mod_path):
+            mod_id = os.path.splitext(mod_file)[0]
+            install_mod(mod_id)
+
 def download_file(url, filename):
-    response = requests.get(url, stream=True)
-    with open(filename, 'wb') as file:
-        total_length = int(response.headers.get('content-length'))
-        for data in tqdm(response.iter_content(chunk_size=1024), total=total_length / 1024, desc="Downloading"):
-            file.write(data)
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        total_size = int(response.headers.get("content-length", 0))
+        progress_bar = tqdm(total=total_size, unit="B", unit_scale=True)
 
-# Function to load the mod configuration from a file
+        with open(filename, "wb") as file:
+            for data in response.iter_content(chunk_size=1024):
+                file.write(data)
+                progress_bar.update(len(data))
+
+        progress_bar.close()
+
+# Function to parse command-line arguments
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Mod Manager CLI")
+    parser.add_argument("command", choices=["install", "uninstall", "update", "search", "view", "enable", "disable", "backup", "load_backup", "details", "total_installed", "exit"], help="Command to execute")
+    parser.add_argument("--mod_id", help="ID of the mod")
+    parser.add_argument("--query", help="Search query for mod search")
+    parser.add_argument("--backup_dir", default=BACKUP_DIRECTORY, help="Directory to store mod backups")
+    return parser.parse_args()
+
+# Function to load mod configuration from file
 def load_config():
     if os.path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE, 'r') as file:
+        with open(CONFIG_FILE, "r") as file:
             return json.load(file)
-    return {}
+    else:
+        return {}
 
-# Function to save the mod configuration to a file
+# Function to save mod configuration to file
 def save_config(mod_config):
-    with open(CONFIG_FILE, 'w') as file:
+    with open(CONFIG_FILE, "w") as file:
         json.dump(mod_config, file)
 
-# Function to update the mod configuration
+# Function to update mod configuration after installation
 def update_config(mod_id, mod_name, mod_version):
     mod_config = load_config()
     mod_config[mod_id] = {
         'name': mod_name,
+        'file': f"{mod_id}.zip",
         'version': mod_version,
-        'filename': f"{mod_id}-{mod_version}.zip",
         'enabled': True
     }
     save_config(mod_config)
 
-# Function to enable or disable a mod
-def enable_mod(mod_id, enable=True):
-    mod_config = load_config()
-    if mod_id in mod_config:
-        mod_config[mod_id]['enabled'] = enable
-        save_config(mod_config)
-        print(f"Mod '{mod_id}' {'enabled' if enable else 'disabled'} successfully.")
-    else:
-        print("Mod ID not found in the configuration.")
-
-# Function to perform backups of installed mods
-def backup_mods(backup_directory):
-    if not os.path.exists(backup_directory):
-        os.makedirs(backup_directory)
-
-    mod_config = load_config()
-    for mod_id, mod_data in mod_config.items():
-        mod_file = mod_data['filename']
-        mod_path = os.path.join(MODS_DIRECTORY, mod_file)
-        if os.path.exists(mod_path):
-            backup_file = os.path.join(backup_directory, mod_file)
-            shutil.copy(mod_path, backup_file)
-            print(f"Mod '{mod_id}' backed up successfully.")
-        else:
-            print(f"Mod file for '{mod_id}' not found.")
-
-# Function to load mods from a backup
-def load_mods_from_backup(backup_directory):
-    if not os.path.exists(backup_directory):
-        print("Backup directory not found.")
-        return
-
-    backup_files = os.listdir(backup_directory)
-    if not backup_files:
-        print("No backup files found.")
-        return
-
-    for backup_file in tqdm(backup_files, desc="Restoring backups"):
-        backup_path = os.path.join(backup_directory, backup_file)
-        mod_destination = os.path.join(MODS_DIRECTORY, backup_file)
-
-        if os.path.exists(mod_destination):
-            print(f"Skipping '{backup_file}' - Mod already exists.")
-            continue
-
-        shutil.copy(backup_path, mod_destination)
-        print(f"Mod '{backup_file}' restored successfully.")
-
-# Function to handle command-line arguments (unchanged)
-def parse_arguments():
-    commands = [
-        "search", "install", "uninstall", "update", "view",
-        "check_updates", "backup", "load_mods", "enable", "disable", "exit"
-    ]
-    parser = argparse.ArgumentParser(description="Mod Manager")
-    parser.add_argument("command", choices=commands, help="Choose a command")
-    parser.add_argument("--mod_id", help="Mod ID")
-    parser.add_argument("--query", help="Search query")
-    return parser.parse_args()
-
-# Main function to handle user commands (unchanged)
+# Main function to handle commands
 def main():
-    if not os.path.exists(MODS_DIRECTORY):
-        os.makedirs(MODS_DIRECTORY)
-
-    if not os.path.exists(BACKUP_DIRECTORY):
-        os.makedirs(BACKUP_DIRECTORY)
-
     args = parse_arguments()
 
-    if args.command == "search":
-        search_mods(args.query)
-    elif args.command == "install":
+    if args.command == "install":
         install_mod(args.mod_id)
     elif args.command == "uninstall":
         uninstall_mod(args.mod_id)
     elif args.command == "update":
         update_mod(args.mod_id)
+    elif args.command == "search":
+        search_mods(args.query)
     elif args.command == "view":
-        view_installed_mods()
-    elif args.command == "check_updates":
-        check_mod_updates()
-    elif args.command == "backup":
-        backup_mods(BACKUP_DIRECTORY)
-    elif args.command == "load_mods":
-        load_mods_from_backup(BACKUP_DIRECTORY)
+        view_enabled_mods() if args.status == "enabled" else view_disabled_mods()
     elif args.command == "enable":
         enable_mod(args.mod_id, enable=True)
     elif args.command == "disable":
         enable_mod(args.mod_id, enable=False)
+    elif args.command == "backup":
+        backup_mods(args.backup_dir)
+    elif args.command == "load_backup":
+        load_mods_from_backup(args.backup_dir)
+    elif args.command == "details":
+        get_mod_details(args.mod_id)
+    elif args.command == "total_installed":
+        get_total_installed_mods()
     elif args.command == "exit":
-        import sys
-        sys.exit()
+        print("Exiting Mod Manager CLI.")
+        return
     else:
-        print("Invalid command.")
+        print("Invalid command. Please try again.")
 
 if __name__ == "__main__":
     main()
